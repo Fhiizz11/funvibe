039a736684c21dd28c225304aa891d9dd1e790bb
Fhiz <148067223+Fhiizz11@users.noreply.github.com>
2025-03-05 02:41:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 // Routing untuk halaman daftar
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+
 
 app.get("/daftar", (req, res) => {
     res.sendFile(path.join(__dirname, "daftar.html"));
